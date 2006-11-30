550709876b42bdd7690366737d25ec9783141400
svn <svn@3608096e-2245-4430-a49f-25e22c201fe7>
2006-11-30 15:17:01 +0000

    EG/JJ: moved setup.py to src dir
    git-svn-id: file:///home/thijs/dev2/weightless2git/weightless-svn/weightless-core/trunk@46 3608096e-2245-4430-a49f-25e22c201fe7

--- setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-
-setup (name = 'weightless',
-       version = '0.1',
-       description = 'Lightweight server framework',
-		package_dir = { 'weightless' : 'src' },
-       packages = ['weightless', 'weightless.wlsocket', 'weightless.wlthread'])
