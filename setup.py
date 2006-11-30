98a3683fc398386087477d35e4ba408fbdf2ceca
svn <svn@3608096e-2245-4430-a49f-25e22c201fe7>
2006-11-30 14:53:35 +0000

    JJ/EG: distutils setup.py added
    git-svn-id: file:///home/thijs/dev2/weightless2git/weightless-svn/weightless-core/trunk@45 3608096e-2245-4430-a49f-25e22c201fe7

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup (name = 'weightless',
+       version = '0.1',
+       description = 'Lightweight server framework',
+		package_dir = { 'weightless' : 'src' },
+       packages = ['weightless', 'weightless.wlsocket', 'weightless.wlthread'])
