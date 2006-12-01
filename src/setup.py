1e494b401babeec1436c80d17308482f007e2a10
svn <svn@3608096e-2245-4430-a49f-25e22c201fe7>
2006-12-01 15:57:28 +0000

    EG: fix path src
    git-svn-id: file:///home/thijs/dev2/weightless2git/weightless-svn/weightless-core/trunk@51 3608096e-2245-4430-a49f-25e22c201fe7

--- src/setup.py
+++ src/setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 setup (name = 'weightless',
 	version = '0.1',
 	description = 'Lightweight server framework',
-	package_dir = { 'weightless' : 'src' },
+	package_dir = { 'weightless' : '.' },
 	packages = ['weightless', 'weightless.wlsocket', 'weightless.wlthread'])
