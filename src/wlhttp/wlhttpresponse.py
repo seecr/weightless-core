f039dad66e465c03792f2dcf9f051275f4911542
svn <svn@3608096e-2245-4430-a49f-25e22c201fe7>
2006-12-08 13:56:22 +0000

    JJ: StatusLine in responses is parsed properly now
    git-svn-id: file:///home/thijs/dev2/weightless2git/weightless-svn/weightless-core/trunk@56 3608096e-2245-4430-a49f-25e22c201fe7

--- src/wlhttp/wlhttpresponse.py
+++ src/wlhttp/wlhttpresponse.py
@@ -1,7 +1,20 @@
+from re import compile
+
+#Status-Line = HTTP-Version SP Status-Code SP Reason-Phrase CRLF
+HTTP_Version = r'HTTP/(?P<version>\d\.\d)'
+Status_Code = r'(?P<statuscode>[0-9]{3})'
+Reason_Phrase = r'(?P<reasonphrase>[^\r\n].+)'
+SP = ' '
+CRLF = '\r\n'
+Status_Line = HTTP_Version + SP + Status_Code + SP + Reason_Phrase + CRLF 
+
+Status_LineRE = compile(Status_Line)
 
 def WlHttpResponse(args):
 	statusLine = yield None
-	args.StatusCode = '200'
-	args.HTTPVersion = '1.1'
-	args.ReasonPhrase = 'Ok'
+	match = Status_LineRE.match(statusLine)
+	if match:
+		args.StatusCode = match.groupdict()['statuscode']
+		args.HTTPVersion = match.groupdict()['version']
+		args.ReasonPhrase = match.groupdict()['reasonphrase']
 	yield None
