52dc47b3835eaa58688b09125ae79d72955687fa
svn <svn@3608096e-2245-4430-a49f-25e22c201fe7>
2006-12-28 15:16:46 +0000

    JJ/TJ: simple http echo server; comments added
    git-svn-id: file:///home/thijs/dev2/weightless2git/weightless-svn/weightless-core/trunk@70 3608096e-2245-4430-a49f-25e22c201fe7

--- examples/httpecho.py
+++ examples/httpecho.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python2.5
 
+"""
+This is a simple Echo server that uses the HTTP Protocol to communicate.
+The "recvRequest" call is capable of parsing HTTP request and will yield
+a dictionary-like object containing the RequestURI, the HTTPVersion and the
+headers sent with the request.
+"""
+
 from server import main
 from weightless.wlhttp import recvRequest
 
 CRLF = "\r\n"
 
 def sendBody(path):
+	result = "PATH: %s" % path
+	yield "HTTP/1.1 200 OK" + CRLF + \
+				"Content-Length: %s" % len(result) + CRLF +\
+				CRLF + \
+				result
 	
-
 def sinkFactory():
 	"""endlessly read and echo back to the client"""
 	while 1:
 		request = yield recvRequest()
-		yield "HTTP/1.1 200 OK" + CRLF +\
-					CRLF +\
-					"PATH: %s" % request.RequestURI + CRLF + 
 		yield sendBody(request.RequestURI)
 
 if __name__ == '__main__':
