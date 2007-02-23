ce8cee15f513d50c367ffd616ffa56eaabbe8e18
TJ <TJ@3608096e-2245-4430-a49f-25e22c201fe7>
2007-02-23 10:42:52 +0000

    TJ/SS: installation and dependency changed.
    git-svn-id: file:///home/thijs/dev2/weightless2git/weightless-svn/weightless-core/trunk@105 3608096e-2245-4430-a49f-25e22c201fe7

--- install/install.sh
+++ install/install.sh
@@ -2,5 +2,12 @@
 
 basedir=$(cd `dirname $0`; pwd)
 
-$basedir/install_python2.5.sh
-$basedir/install_sendfile.sh
+source $basedir/functions.sh
+
+aptitude install python2.5
+echo "*
+* We will now make python 2.5 the default python version."
+cd /usr/bin
+rm python
+ln -s python2.5 python
+aptitude_install http://debian.cq2.org stable main python-sendfile
