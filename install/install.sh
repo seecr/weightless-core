d8d81f271bbd3359190c7b3c9663b3deeb48abea
TJ <TJ@3608096e-2245-4430-a49f-25e22c201fe7>
2007-04-12 15:05:01 +0000

    TJ/JJ: install.
    git-svn-id: file:///home/thijs/dev2/weightless2git/weightless-svn/weightless-core/trunk@120 3608096e-2245-4430-a49f-25e22c201fe7

--- install/install.sh
+++ install/install.sh
@@ -11,3 +11,11 @@ cd /usr/bin
 rm python
 ln -s python2.5 python
 aptitude_install http://debian.cq2.org stable main python-sendfile
+
+if [ -z "$(which easy_install-2.5)" ]; then
+  cd /tmp
+  wget http://peak.telecommunity.com/dist/ez_setup.py
+  python ez_setup.py
+  rm ez_setup.py
+fi
+easy_install-2.5 Pyrex==0.9.5.1a
