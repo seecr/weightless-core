169a2aba8b422e1452e789d69b0e6478934ce8d9
JJ <JJ@3608096e-2245-4430-a49f-25e22c201fe7>
2007-04-13 07:36:19 +0000

    TJ/JJ: added method to functions.
    git-svn-id: file:///home/thijs/dev2/weightless2git/weightless-svn/weightless-core/trunk@121 3608096e-2245-4430-a49f-25e22c201fe7

--- install/functions.sh
+++ install/functions.sh
@@ -16,3 +16,19 @@ function isroot {
  fi
 }
 
+
+function aptitude_install {
+	repository=$1
+	distro=$2
+	component=$3
+	shift 3
+	package=$*
+	APTFILE=/etc/apt/sources.list.d/$distro'_'$component.list
+	message "Adding temporary repository to APT ($repository $distro $component)"
+	echo "deb $repository $distro $component" > $APTFILE
+	aptitude update
+	aptitude install $package
+	message "Removing temporary repository ($repository $distro $component)"
+	rm $APTFILE
+	aptitude update
+}
