15a2d480685660787611f4a1628c575d39cc637b
EG <EG@3608096e-2245-4430-a49f-25e22c201fe7>
2007-07-24 11:11:54 +0000

    EG: finalized setup.py installation
    git-svn-id: file:///home/thijs/dev2/weightless2git/weightless-svn/weightless-core/trunk@129 3608096e-2245-4430-a49f-25e22c201fe7

--- src/wlcompose/__init__.py
+++ src/wlcompose/__init__.py
@@ -1,6 +1,17 @@
 from os import system
-from os.path import dirname
+from os.path import dirname, isfile, join, getmtime
 
-system("cd %s; python2.5 setup.py build_ext --inplace" % dirname(__file__))
+mydir = dirname(__file__)
+solib = join(mydir, 'compose.so')
+pyxsource = join(mydir, 'compose.pyx')
+
+if not isfile(solib) or getmtime(solib) < getmtime(pyxsource):
+    print '* Extension compose.so not found or outdated.'
+    print '* You are either in a development environment or'
+    print '* your installation of weightless is broken. Assuming'
+    print '* the former.'
+    print '* Now compiling compose.so extension.', mydir
+    system("cd %s; python2.5 setup.py build_ext --inplace" % mydir)
 
 from compose import compose, RETURN
+
