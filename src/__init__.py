1579390c01785629d3a54ba4047808d55f8e99fb
EG <EG@3608096e-2245-4430-a49f-25e22c201fe7>
2007-09-10 13:49:22 +0000

    EG: removed python 2,5 stuff
    git-svn-id: file:///home/thijs/dev2/weightless2git/weightless-svn/weightless-core/trunk@150 3608096e-2245-4430-a49f-25e22c201fe7

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,11 @@
-from _compose_py import compose, RETURN
+#from _compose_py import compose, RETURN
 #from _pool import Pool
 #from _socket import Socket
 #from _select import Select
 #from _service import Service
 #from _template import Template
 
+#python 2.4 stuff
 from _acceptor import Acceptor
 from _reactor import Reactor
 from _httpreader import HttpReader
