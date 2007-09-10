4634f133c502b6fbbb2e9787f0b2c677adad6680
EG <EG@3608096e-2245-4430-a49f-25e22c201fe7>
2007-09-10 13:42:18 +0000

    EG: forgot _acceptor
    git-svn-id: file:///home/thijs/dev2/weightless2git/weightless-svn/weightless-core/trunk@149 3608096e-2245-4430-a49f-25e22c201fe7

--- src/_acceptor.py
+++ src/_acceptor.py
@@ -2,13 +2,15 @@ from socket import socket, AF_INET, SOCK_STREAM
 
 class Acceptor(object):
 
-    def __init__(self, port, reactor, sinkFactory):
+    def __init__(self, reactor, port, sinkFactory):
         sok = socket(AF_INET, SOCK_STREAM)
         sok.bind(('0.0.0.0', port))
         sok.listen(1)
         reactor.addReader(sok, self.accept)
         self.sinkFactory = sinkFactory
+        self._sok = sok
+        self._reactor = reactor
 
-    def accept(self, reactor, sok):
-        newConnection, address = sok.accept()
-        reactor.addReader(newConnection, self.sinkFactory(reactor, newConnection))
+    def accept(self):
+        newConnection, address = self._sok.accept()
+        self._reactor.addReader(newConnection, self.sinkFactory(newConnection))
