1b09b25044aed1e23909f6539fd57777d80d7380
JJ <JJ@3608096e-2245-4430-a49f-25e22c201fe7>
2009-03-31 08:58:08 +0000

    JJ: added O to certificate
    git-svn-id: file:///home/thijs/dev2/weightless2git/weightless-svn/weightless-core/trunk@369 3608096e-2245-4430-a49f-25e22c201fe7

--- test/ssl/mk_simple_certs.py
+++ test/ssl/mk_simple_certs.py
@@ -11,13 +11,14 @@ def generateIn(directory):
     cakey = createKeyPair(TYPE_RSA, 1024)
     careq = createCertRequest(cakey, CN='Certificate Authority')
     cacert = createCertificate(careq, (careq, cakey), 0, (0, 60*60*24*365*5)) # five years
-    open(join(directory, 'CA.pkey'), 'w').write(crypto.dump_privatekey(crypto.FILETYPE_PEM, cakey))
-    open(join(directory, 'CA.cert'), 'w').write(crypto.dump_certificate(crypto.FILETYPE_PEM, cacert))
+    #open(join(directory, 'CA.pkey'), 'w').write(crypto.dump_privatekey(crypto.FILETYPE_PEM, cakey))
+    #open(join(directory, 'CA.cert'), 'w').write(crypto.dump_certificate(crypto.FILETYPE_PEM, cacert))
 
 
     CN = raw_input("CN: ").strip()
+    O = raw_input("O: ").strip()
     pkey = createKeyPair(TYPE_RSA, 1024)
-    req = createCertRequest(pkey, CN=CN)
+    req = createCertRequest(pkey, CN=CN, O=O)
     cert = createCertificate(req, (cacert, cakey), 1, (0, 60*60*24*365*5)) # five years
     open(join(directory, 'server.pkey'), 'w').write(crypto.dump_privatekey(crypto.FILETYPE_PEM, pkey))
     open(join(directory, 'server.cert'), 'w').write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
