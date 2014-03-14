a4af28d078a0587a8d3cc4631675df3ed3fbc1e1
Hendrik Moree <hendrik@seecr.nl>
2014-03-14 16:06:28 +0100

    TJ/HM: Don't compress if data is from self._rest; It's already compressed

--- weightless/http/_httpserver.py
+++ weightless/http/_httpserver.py
@@ -1,27 +1,27 @@
 # -*- coding: utf-8 -*-
 ## begin license ##
-# 
-# "Weightless" is a High Performance Asynchronous Networking Library. See http://weightless.io 
-# 
+#
+# "Weightless" is a High Performance Asynchronous Networking Library. See http://weightless.io
+#
 # Copyright (C) 2006-2011 Seek You Too (CQ2) http://www.cq2.nl
-# Copyright (C) 2011-2012 Seecr (Seek You Too B.V.) http://seecr.nl
-# 
+# Copyright (C) 2011-2012, 2014 Seecr (Seek You Too B.V.) http://seecr.nl
+#
 # This file is part of "Weightless"
-# 
+#
 # "Weightless" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Weightless" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Weightless"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from _acceptor import Acceptor
@@ -215,7 +215,7 @@ def updateResponseHeaders(headers, match, addHeaders=None, removeHeaders=None, r
     for header in removeHeaders:
         headerRe = _removeHeaderReCache.get(header, None)
         if headerRe is None:
-            
+
             headerRe = re.compile(r'\r\n%s:.*?\r\n' % re.escape(header), flags=re.I)
             _removeHeaderReCache[header] = headerRe
         _headers = headerRe.sub(CRLF, _headers, count=1)
@@ -509,7 +509,7 @@ class HttpHandler(object):
                                 data = _statusLineAndHeaders + encodeResponseBody.compress(_bodyStart)
                         else:
                             continue
-                    else:
+                    elif not self._rest:
                         data = encodeResponseBody.compress(data)
 
                 if data:  # TODO find a unittest for it; if you can
