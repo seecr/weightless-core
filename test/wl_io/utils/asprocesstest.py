c41ca04ebd847ccb972744a4f32785abbeb28ec1
Thijs Janssen <thijs@seecr.nl>
2015-04-30 11:11:30 +0200

    TS/TJ: added asProcess(<aGenerator>)
    Allows scripting-style code-writing; while the generator contains
    async code.
    Roughly; the generator gets composed, executed as a process in a wrapper-driver,
    and the reactor stops when all async code is executed (or on an exception).
    Works also as a decorator and returns the async retval as a normal retval.

---
+++ test/wl_io/utils/asprocesstest.py
@@ -0,0 +1,290 @@
+## begin license ##
+#
+# "Weightless" is a High Performance Asynchronous Networking Library. See http://weightless.io
+#
+# Copyright (C) 2015 Seecr (Seek You Too B.V.) http://seecr.nl
+#
+# This file is part of "Weightless"
+#
+# "Weightless" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# "Weightless" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "Weightless"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+## end license ##
+
+from seecr.test import SeecrTestCase, CallTrace
+from seecr.test.io import stdout_replaced
+
+from functools import partial
+
+from weightless.core import identify, local
+from weightless.io import Reactor, reactor, Suspend
+
+from weightless.io.utils import asProcess
+
+
+class AsProcessTest(SeecrTestCase):
+    def testOnlyAcceptsAGeneratorOrGeneratorFunction(self):
+        self.assertRaises(TypeError, lambda: asProcess())
+        self.assertRaises(TypeError, lambda: asProcess(object()))
+        def functionNotReturningAGenerator(whatever):
+            return [whatever]
+        self.assertRaises(TypeError, lambda: asProcess(functionNotReturningAGenerator)('whatever'))
+
+        # Readable message
+        try:
+            asProcess(functionNotReturningAGenerator)('whatever')
+            self.fail()
+        except TypeError, e:
+            self.assertEquals("asProcess() expects a generator, got ['whatever']", str(e))
+
+        # decorator
+        @asProcess
+        def genFDecor():
+            raise StopIteration('ok')
+            yield
+        self.assertEquals('ok', genFDecor())
+
+        # arguments passed into and returned as retval (normal and as decorator)
+        def genF(a, b):
+            raise StopIteration((a, b))
+            yield
+        self.assertEquals((1, 2), asProcess(genF)(1, b=2))
+        self.assertEquals((1, 2), asProcess(genF(1, b=2)))
+
+    def testDecorator(self):
+        @asProcess
+        def g():
+            raise StopIteration(1)
+            yield
+        self.assertEquals(1, g())
+
+        class A(object):
+            @staticmethod
+            @asProcess
+            def g():
+                raise StopIteration(3)
+                yield
+        self.assertEquals(3, A.g())
+
+        def g(a):
+            raise StopIteration(a)
+            yield
+        # Below does not work; because partials' output does not look like
+        # a normal function.
+        # Could be solved with a callable(g) check; though then @warps
+        # does not work (expects attributes to be there - like a function).
+        #
+        # TS: Not worth the effort - just feed the generator (don't decorate)
+        #     when using callable non-functions.
+        self.assertRaises(TypeError, lambda: asProcess(partial(g, a=2)))
+
+    def testMultipleSerialAsProcesses(self):
+        events = []
+        def multiple():
+            def one():
+                events.append('1 start')
+                yield '/dev/null (1)'
+                events.append('1 yielded')
+                result = yield one_2()
+                events.append('1 delegated')
+                raise StopIteration(result)
+            def one_2():
+                events.append('1-2 called')
+                raise StopIteration(1)
+                yield
+            def two():
+                events.append('2 start')
+                yield '/dev/null (2)'
+                events.append('2 yielded')
+                raise StopIteration(2)
+
+            res1 = asProcess(one())
+            res2 = asProcess(two())
+
+            return (res1, res2)
+
+        self.assertEquals([], events)
+
+        result = multiple()
+        self.assertEquals((1, 2), result)
+        self.assertEquals([
+                '1 start',
+                '1 yielded',
+                '1-2 called',
+                '1 delegated',
+                '2 start',
+                '2 yielded',
+            ], events)
+
+    def testMultipleNestedAsProcesses(self):
+        @asProcess
+        def a():
+            retval = b()
+            raise StopIteration(retval)
+            yield
+        @asProcess
+        def b():
+            retval = c()
+            raise StopIteration(retval)
+            yield
+        @asProcess
+        def c():
+            raise StopIteration('c')
+            yield
+
+        self.assertEquals('c', a())
+
+    def testAsProcessInAsyncGeneratorBehavesLikeASynchrounusFunction(self):
+        # Internals not leaked, different Reactor, blocking!
+        # White-box test; assumes all reactor methods explicitly used are intercepted & passed on to the realReactor.
+        realReactor = reactor = Reactor()
+        mockReactor = CallTrace("MockReactor", methods={
+                'addProcess': realReactor.addProcess,
+                'removeProcess': realReactor.removeProcess,
+                'addTimer': realReactor.addTimer,
+            }
+        )
+        realReactor.addProcess = mockReactor.addProcess
+        realReactor.removeProcess = mockReactor.removeProcess
+        realReactor.addTimer = mockReactor.addTimer
+
+        def a():
+            raise StopIteration((yield b()))
+            yield
+        def b():
+            raise StopIteration('retval')
+            yield
+
+        @identify
+        def normalAsync():
+            this = yield
+            reactor.addProcess(this.next)
+            yield # waiting for realReactor's step()
+            result = asProcess(a())
+            yield # waiting for another step()
+            reactor.removeProcess(this.next)
+            raise Exception(('Stop', result))
+
+        def raiser():
+            raise AssertionError('Failwhale!')
+        reactor.addTimer(0.3, raiser)
+        normalAsync()
+        try:
+            reactor.loop()
+            self.fail()
+        except AssertionError:
+            raise
+        except Exception, e:
+            self.assertEquals(('Stop', 'retval'), e.args[0])
+
+        self.assertEquals(['addTimer', 'addProcess', 'removeProcess'], mockReactor.calledMethodNames())
+
+    def testAsyncReturnValuePassing(self):
+        def noRetval():
+            return
+            yield
+        def noneRetval():
+            raise StopIteration(None)
+            yield
+        def oneRetval():
+            raise StopIteration(1)
+            yield
+        def oneRetvalWithIgnoredPushback():
+            raise StopIteration(1, 'p', 'u', 's', 'h')
+            yield
+        def retvalDependantOnPushbackEval():
+            one_ = yield oneRetvalWithIgnoredPushback()
+            for _ in ['p', 'u', 's']:
+                _ = yield
+            raise StopIteration((yield))
+        self.assertEquals(None, asProcess(noRetval()))
+        self.assertEquals(None, asProcess(noneRetval()))
+        self.assertEquals(1, asProcess(oneRetval()))
+        self.assertEquals(1, asProcess(oneRetvalWithIgnoredPushback()))
+        self.assertEquals('h', asProcess(retvalDependantOnPushbackEval()))
+
+    def testDecoratedFunctionRaises(self):
+        def raiser():
+            raise Exception('Boom!')
+        self.assertRaises(Exception, lambda: asProcess(raiser)())
+
+    def testNormalExceptionHandlingFromAsync(self):
+        def g():
+            raise Exception('Boom!')
+            yield
+        self.assertRaises(Exception, lambda: asProcess(g()))
+
+        def g():
+            raise BaseException('Boom!')
+            yield
+        self.assertRaises(BaseException, lambda: asProcess(g()))
+
+    def testGeneratorOrReactorSpecialsExceptionHandlingFromAsync(self):
+        # GeneratorExit, (AssertionError, KeyboardInterrupt, SystemExit); all still quit-the-loop; ...
+        # shutdown call cannot "hang"; ...
+        def gexit():
+            raise GeneratorExit()
+            yield
+        self.assertRaises(GeneratorExit, lambda: asProcess(gexit()))
+
+        def asserr():
+            raise AssertionError()
+            yield
+        self.assertRaises(AssertionError, lambda: asProcess(asserr()))
+
+        def keyint():
+            raise KeyboardInterrupt()
+            yield
+        self.assertRaises(KeyboardInterrupt, lambda: asProcess(keyint()))
+
+        def sysexit():
+            raise SystemExit()
+            yield
+        self.assertRaises(SystemExit, lambda: asProcess(sysexit()))
+
+    def testUnfinishedBusinessInReactorLogs(self):
+        neverCalled = []
+        def timeAfterFinished():
+            assert local('__reactor__') is reactor()  # Code explaining code :-)
+            currentReactor = reactor()
+            currentReactor.addTimer(1.0, lambda: neverCalled.append(True))
+            yield 'async work...'
+            currentReactor.addProcess(lambda: neverCalled.append(True))
+            raise StopIteration(42)
+
+        with stdout_replaced() as out:
+            result = asProcess(timeAfterFinished())
+            self.assertEquals(42, result)
+            self.assertTrue('Reactor shutdown: terminating <function' in out.getvalue(), out.getvalue())
+
+    def testSuspendProtocolForCallables(self):
+        @identify
+        def whileSuspended():
+            this = yield
+            suspend = yield
+            suspend._reactor.addProcess(this.next)
+            yield
+            suspend._reactor.removeProcess(this.next)
+            suspend.resume('whileSuspended')
+            yield # Wait for GC
+        def suspending():
+            s = Suspend(whileSuspended().send)
+            yield s
+            raise StopIteration(s.getResult())
+        def thisAndThat():
+            this = '42'
+            that = yield suspending()
+            raise StopIteration((this, that))
+        self.assertEquals(('42', 'whileSuspended'), asProcess(thisAndThat()))
+
---
+++ weightless/io/utils/asprocess.py
@@ -0,0 +1,76 @@
+## begin license ##
+#
+# "Weightless" is a High Performance Asynchronous Networking Library. See http://weightless.io
+#
+# Copyright (C) 2015 Seecr (Seek You Too B.V.) http://seecr.nl
+#
+# This file is part of "Weightless"
+#
+# "Weightless" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# "Weightless" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "Weightless"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+## end license ##
+
+from functools import wraps
+from types import FunctionType
+
+from weightless.core import compose, Yield, identify, is_generator
+from weightless.io import Reactor
+
+
+def asProcess(g):
+    """
+    Facillitate scripting with Components, DNA, and other async jazz; without the headace.
+
+    Accepts a generator, composes and runs is using a Reactor's addProcess and loop, quits loop and returns the async return-value at end-of-generator.
+    """
+    reactor = Reactor()
+
+    def _asProcess(g):
+        if not is_generator(g):
+            raise TypeError("asProcess() expects a generator, got %s" % repr(g))
+
+        wrapper(g)
+        try:
+            reactor.loop()
+        except StopIteration, e:
+            if e.args:
+                return e.args[0]
+
+    @identify
+    def wrapper(generator):
+        this = yield
+        reactor.addProcess(process=this.next)
+        try:
+            yield
+            g = compose(generator)
+            while True:
+                _response = g.next()
+                if _response is Yield:
+                    continue
+                if _response is not Yield and callable(_response):
+                    _response(reactor, this.next)
+                    yield
+                    _response.resumeProcess()
+                yield
+        finally:
+            reactor.removeProcess(process=this.next)
+
+    if type(g) == FunctionType:  # asProcess used as decorator
+        @wraps(g)
+        def helper(*args, **kwargs):
+            return _asProcess(g(*args, **kwargs))
+        return helper
+    return _asProcess(g)
+
