52c6aa29d76895d428e738567652f25fa43f0f92
Erik Groeneveld <erik@seecr.nl>
2021-02-15 14:13:44 +0100

    EG: formatting only

--- test/core/utilstest.py
+++ test/core/utilstest.py
@@ -1,25 +1,25 @@
 ## begin license ##
-# 
-# "Weightless" is a High Performance Asynchronous Networking Library. See http://weightless.io 
-# 
+#
+# "Weightless" is a High Performance Asynchronous Networking Library. See http://weightless.io
+#
 # Copyright (C) 2011-2012 Seecr (Seek You Too B.V.) http://seecr.nl
-# 
+#
 # This file is part of "Weightless"
-# 
+#
 # "Weightless" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Weightless" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Weightless"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from unittest import TestCase
@@ -29,7 +29,7 @@ from functools import partial
 class UtilsTest(TestCase):
     def testIsGeneratorFunction(self):
         def a(x,y,z):
-            yield 'a' 
+            yield 'a'
 
         class B(object):
             def method(self):
@@ -44,7 +44,7 @@ class UtilsTest(TestCase):
                 yield 'b'
 
         def c(x,y,z):
-            return 'c' 
+            return 'c'
 
         class D(object):
             def method(self):
@@ -69,10 +69,10 @@ class UtilsTest(TestCase):
 
     def testIsGeneratorFunctionForPartial(self):
         def a(x,y,z):
-            yield 'a' 
+            yield 'a'
 
         def c(x,y,z):
-            return 'c' 
+            return 'c'
 
         e = partial(a, 1)
         f = partial(c, 1)
